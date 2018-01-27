5b744987df469e700f3ca0584ffa55ea558db061
The Coder School <tcsbcoach@gmail.com>
2018-01-27 12:01:53 -0800

    sorry, my stapler instincts took over

--- sta.py
+++ sta.py
@@ -1,7 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
-url = "https://akshatmittal.com/youtube-realtime/#!/UC9z7EZAbkphEMg0SP7rw44A"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+import re
+import os
+from datetime import datetime
+import time
+def count(user):
+	url = "https://socialblade.com/youtube/user/"+user+"/realtime"
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	#print(soup)
+	digits = soup.find(id="rawCount")
+	#digits = soup.find_all("span", attrs={"class": "odometer-value"})
+	#print(type(digits))
+	#return re.findall(r"\d+", digits).group(0)
+	return str(digits.string)
 
-digits = soup.find_all(class="odometer-value")
+def log(user, n, t):
+	mode = "a" if os.path.isfile("./"+user+".csv") else "w"
+	with open("./"+user+".csv", mode) as f:
+		for i in range(n):
+			#f.write(str(datetime.now())+","+count(user)+"\n")
+			f.write(str(i)+"\n")
+			print(i)
+			time.sleep(t)
+
+log("carykh", 100, 2)
